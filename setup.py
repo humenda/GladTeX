a8e3d0e748ca4f1bc055c41005d9d4bf036f8364
Sebastian Humenda <shumenda |at| gmx |dot| de>
2015-10-30 09:42:43 +0100

    install script as gladtex, not gladtex.py into path
    When installing the GladTeX with .py iextension, users would not find it since
    they'd expect the program to be called `gladtex`.

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
+import distutils.command.install_scripts
 from distutils.core import setup
 from gleetex import VERSION
+import shutil
+import sys
+
+class my_install(distutils.command.install_scripts.install_scripts):
+    """Custom script installer. Stript .py extension if not on Windows."""
+    def run(self):
+        distutils.command.install_scripts.install_scripts.run(self)
+        for script in self.get_outputs():
+            if script.endswith(".py") and not ('wind' in sys.platform or 'win32'
+                    in sys.platform):
+                # strip file ending (if not on windows) to make it executable as
+                # a command
+                shutil.move(script, script[:-3])
+
 
 setup(name='GladTeX',
       version=VERSION,
@@ -9,7 +24,8 @@ setup(name='GladTeX',
       url='https://humenda.github.io/GladTeX',
       packages=['gleetex'],
       scripts=['gladtex.py'],
-      license = "LGPL3.0"
+      license = "LGPL3.0",
+      cmdclass = {"install_scripts": my_install}
      )
 
 
