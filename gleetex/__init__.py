4c9505f03dd0bb0e2cc4d4e4860cd555bc5001a6
Sebastian Humenda <shumenda |at| gmx |dot| de>
2016-09-06 18:59:59 +0200

    update version number

--- gleetex/__init__.py
+++ gleetex/__init__.py
@@ -4,7 +4,7 @@ from . import document
 from . import htmlhandling
 from . import image
 
-VERSION = '2.0'
+VERSION = '2.0.1'
 
 __all__ = ['caching', 'convenience', 'document', 'htmlhandling', 'image',
         'VERSION']
