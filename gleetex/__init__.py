ecf7444d9e048586ad6c1b8407ed0e66478e9bd6
Sebastian Humenda <shumenda@gmx.de>
2018-09-13 16:04:02 +0200

    bump version number

--- gleetex/__init__.py
+++ gleetex/__init__.py
@@ -6,7 +6,7 @@ from . import pandoc
 from . import parser
 from . import typesetting
 
-VERSION = '2.4.0'
+VERSION = '3.0.0'
 
 __all__ = ['caching', 'cachedconverter', 'document', 'htmlhandling', 'image',
         'pandoc', 'parser', 'unicode', 'VERSION']
